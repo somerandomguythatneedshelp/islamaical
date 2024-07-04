c74d0c8ffcd928b51bba207436f01509368dfc17
somerandomguythatneedshelp <107365137+somerandomguythatneedshelp@users.noreply.github.com>
2024-07-04 16:13:55 +0100

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,25 +3,8 @@ import Box from './components/Box';
 import NamazIMG from './Images/namaz.png';
 import QuranIMG from './Images/Quran.png';
 import Link from 'next/link';
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 
 export default function Home() {
-
-  const firebaseConfig = {
-    apiKey: "AIzaSyC6tlpefW_xsLClEIctgJa9PUrxRsaeIwY",
-    authDomain: "islamaical.firebaseapp.com",
-    projectId: "islamaical",
-    storageBucket: "islamaical.appspot.com",
-    messagingSenderId: "896740515607",
-    appId: "1:896740515607:web:102dfc2bac074198bb6152",
-    measurementId: "G-561C11FN26"
-  };
-  
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
